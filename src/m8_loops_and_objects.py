9794b8bf2fb780c7c47ad682e8db316bd118983a
Ricardo Hernandez <hernanre@rose-hulman.edu>
2018-09-08 13:51:35 -0400

    Session 3 M9b Complete.

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -280,5 +280,7 @@ def draw_cosines_and_sines(n):
 # If this module is running at the top level (as opposed to being
 # imported by another module), then call the 'main' function.
 # ----------------------------------------------------------------------
+
+
 if __name__ == '__main__':
     main()
